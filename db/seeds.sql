c5414afe2f04d85398c6ca954231d46acd98ba51
kyle <kyle@kyle.nyc>
2020-11-05 22:37:08 -0500

    working structure.. database and data is available but handlebars is not populating the page

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burgerName) VALUES ('Classic Burger');
+INSERT INTO burgers (burgerName) VALUES ('Cheese Burger');
+INSERT INTO burgers (burgerName, devoured) VALUES ('Double Burger', true);
+INSERT INTO burgers (burgerName, devoured) VALUES ('Triple Burger', true);
+INSERT INTO burgers (burgerName, devoured) VALUES ('Special Burger', true);
+INSERT INTO burgers (burgerName) VALUES ('Guac Burger');
